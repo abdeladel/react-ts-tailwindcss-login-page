5e259aa5df0cc112388a6cbc9c0bd0e333fa56d6
abdeladel <majdeb.abdeladel@gmail.com>
2023-05-05 10:17:03 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import Login from './login';
+import './style.css';
+
+export default function App() {
+  return <Login />;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-tailwindcss-login-page
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-vrnqxu)
